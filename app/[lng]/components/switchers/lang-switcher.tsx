f511dfe10e7d8888e1cfbf94c56ff034f890a297
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-16 00:20:26 +0300

    Language switcher

---
+++ app/[lng]/components/switchers/lang-switcher.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Toggle } from "../toggle"
+import { useEffect, useState } from "react"
+import { useParams, usePathname, useRouter } from "next/navigation"
+
+export const LangSwitcher = () => {
+    const [isActive, setIsActive] = useState<boolean>(false)
+
+    const route = useRouter()
+    const params = useParams()
+    const pathname = usePathname()
+
+    const lng = params.lng
+
+    const switchLang = () => {
+        setTimeout(() => {
+            const targetLang = lng === 'en' ? 'ru' : 'en';
+            const newPath = pathname.replace(/\/(en|ru)(\/|$)/, `/${targetLang}$2`);
+
+            route.push(newPath);
+        }, 400, setIsActive(!isActive));
+    }
+
+    useEffect(() => {
+        lng === 'en' ? setIsActive(true) : setIsActive(false)
+    }, [lng])
+
+    return (
+        <Toggle isActive={isActive} func={switchLang} />
+    )
+}
