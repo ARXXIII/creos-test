1e538ad0a84a2389db3ab8c707d551e2ba48e8f8
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-16 00:24:53 +0300

    Tasks page
    TODO: Tasks page

---
+++ app/[lng]/tasks/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const TasksPage = ({ params: { lng } }: { params: { lng: string } }) => {
+
+    return (
+        <>
+            <section>
+            </section>
+        </>
+    )
+}
+
+export default TasksPage
