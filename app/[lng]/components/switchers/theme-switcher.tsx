8e1c74c26a976dc8227fd5b38b360ec97af47811
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-16 00:20:35 +0300

    Theme switcher

---
+++ app/[lng]/components/switchers/theme-switcher.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Toggle } from "../toggle"
+import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
+
+export const ThemeSwitcher = () => {
+
+    const { theme, setTheme, } = useTheme()
+
+    const [isActive, setIsActive] = useState<boolean>(false)
+
+    const switchTheme = () => {
+
+        if (isActive) {
+            setTheme("light")
+        } else {
+            setTheme("dark")
+        }
+
+        setIsActive(!isActive)
+    }
+
+    useEffect(() => {
+        theme === 'dark' ? setIsActive(true) : setIsActive(false)
+    }, [theme])
+
+    return (
+        <Toggle isActive={isActive} func={() => switchTheme()} />
+    )
+}
