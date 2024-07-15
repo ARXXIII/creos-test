24a1fae8dba96cbf8d83ad9787c4a5987d20afba
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-16 00:18:20 +0300

    Filters

---
+++ app/[lng]/components/designers/filters/filter-by-name.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from "react"
+import { ArrowDownAZ, ArrowUpAZ } from "lucide-react"
+
+export const FilterByName = () => {
+    const [filter, setFilter] = useState<'asc' | 'desc'>('asc')
+
+    const handleOnClick = () => {
+        if (filter === 'asc') {
+            setFilter('desc')
+        }
+
+        if (filter === 'desc') {
+            setFilter('asc')
+        }
+    }
+
+    return (
+        <button
+            onClick={handleOnClick}
+            className="flex justify-center items-center gap-x-3 p-3 font-bold text-neutral-500 dark:text-neutral-400 bg-neutral-200 dark:bg-zinc-800 rounded-xl shadow-neumorphism-light dark:shadow-neumorphism-dark active:scale-[0.96] duration-200 ease-in-out"
+        >
+            <h1>By name</h1>
+            {filter === 'asc' ? (<ArrowDownAZ />) : (<ArrowUpAZ />)}
+        </button>
+    )
+}
