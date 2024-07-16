5e327863d6e813765adccfe645d41ee931564e6a
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-16 18:08:09 +0300

    Added multilang & swapped top and comments

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,12 +1,73 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { Comment } from './components/comment'
+import { useEffect, useState } from 'react'
 import { useTranslation } from '../i18n/client'
+
+import { Comment } from './components/comment'
 import { TopItem } from './components/top-item'
+
+interface Comments {
+    id: number
+    issue: string
+    designer: {
+        username: string
+        avatar: string
+    }
+    message: string
+    date_created: string
+}
+
+interface Top {
+    designer: string
+}
 
 const Page = ({ params: { lng } }: { params: { lng: string } }) => {
     const { t } = useTranslation(lng, 'home')
+
+    const [top, setTop] = useState<Array<Top>>()
+    const [comments, setComments] = useState<Array<Comments>>()
+
+    const fetchComments = async () => {
+        try {
+            const response = await fetch('/api/comments');
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            const lastComments = data.slice(0, 10)
+
+            setComments(lastComments)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const fetchTop = async () => {
+        try {
+            const response = await fetch('/api/top');
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            const bestOfTheBest = data.slice(0, 10)
+
+            setTop(bestOfTheBest)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchComments()
+        fetchTop()
+    }, [])
 
     return (
         <>
@@ -18,38 +79,26 @@ const Page = ({ params: { lng } }: { params: { lng: string } }) => {
                         transition={{ type: 'spring', }}
                         className='font-bold text-2xl lg:text-4xl shrink-0'
                     >
-                        {t('title')}
+                        {t('top')}
                     </motion.h1>
                 </div>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
-                    <Comment
-                        name='Name'
-                        img='https://sandbox.creos.me/media/images/avatars/marcusshah.jpg'
-                        task='RR3-320'
-                        message='GedSsGgsWOUpKqCPmZNG'
-                        time='Час назад'
-                    />
-                    <Comment
-                        name='Name'
-                        img='https://sandbox.creos.me/media/images/avatars/marcusshah.jpg'
-                        task='RR3-320'
-                        message='GedSsGgsWOUpKqCPmZNG'
-                        time='Час назад'
-                    />
-                    <Comment
-                        name='Name'
-                        img='https://sandbox.creos.me/media/images/avatars/marcusshah.jpg'
-                        task='RR3-320'
-                        message='GedSsGgsWOUpKqCPmZNG'
-                        time='Час назад'
-                    />
-                    <Comment
-                        name='Name'
-                        img='https://sandbox.creos.me/media/images/avatars/marcusshah.jpg'
-                        task='RR3-320'
-                        message='GedSsGgsWOUpKqCPmZNG'
-                        time='Час назад'
-                    />
+                <div>
+                    {top ? (
+                        <div className='grid grid-cols-2 lg:grid-cols-5 gap-6'>
+
+                            {top.map((designer, index) => (
+                                <TopItem key={designer.designer} name={designer.designer} position={index + 1} />
+                            ))}
+
+                        </div>
+                    ) : <motion.div
+                        initial={{ y: 50, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ duration: 1 }}
+                        className='flex justify-center items-center'
+                    >
+                        <h1 className='p-3 text-2xl bg-neutral-200 dark:bg-zinc-800 rounded-xl shadow-neumorphism-light dark:shadow-neumorphism-dark'>{t('noTop')}</h1>
+                    </motion.div>}
                 </div>
             </section>
             <section className='space-y-6'>
@@ -60,15 +109,32 @@ const Page = ({ params: { lng } }: { params: { lng: string } }) => {
                         transition={{ type: 'spring', }}
                         className='font-bold text-2xl lg:text-4xl shrink-0'
                     >
-                        {t('top')}
+                        {t('title')}
                     </motion.h1>
                 </div>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
-                    <TopItem name='marcusshah' position={1} />
-                    <TopItem name='marcusshah' position={2} />
-                    <TopItem name='marcusshah' position={3} />
-                    <TopItem name='marcusshah' position={4} />
-                    <TopItem name='marcusshah' position={5} />
+                <div className='space-y-6'>
+
+                    {comments ? comments.map((comment) => (
+                        <Comment
+                            key={comment.id}
+                            lng={lng}
+                            name={comment.designer.username}
+                            img={comment.designer.avatar}
+                            task={comment.issue}
+                            message={comment.message}
+                            time={comment.date_created}
+                        />
+                    )) : (
+                        <motion.div
+                            initial={{ y: 50, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 1 }}
+                            className='flex justify-center items-center'
+                        >
+                            <h1 className='p-3 text-2xl bg-neutral-200 dark:bg-zinc-800 rounded-xl shadow-neumorphism-light dark:shadow-neumorphism-dark'>{t('noComments')}</h1>
+                        </motion.div>
+                    )}
+
                 </div>
             </section>
         </>
