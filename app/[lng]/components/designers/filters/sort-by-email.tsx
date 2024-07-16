866ac384fdaaad8a8c0b5b2b0555ba374ba847de
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-16 18:11:47 +0300

    Made the title more semantic & added sorting function

---
+++ app/[lng]/components/designers/filters/sort-by-email.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { ArrowDownAZ, ArrowUpAZ } from "lucide-react"
+
+interface SortByEmailProps {
+    title: string
+    type: string
+    sort: () => void
+}
+
+export const SortByEmail = ({ title, type, sort }: SortByEmailProps) => {
+    return (
+        <button
+            onClick={() => sort()}
+            className="flex justify-center items-center gap-x-3 p-3 font-bold text-neutral-500 dark:text-neutral-400 bg-neutral-200 dark:bg-zinc-800 rounded-lg shadow-neumorphism-light dark:shadow-neumorphism-dark active:scale-[0.96] duration-200 ease-in-out"
+        >
+            <h1>{title}</h1>
+            {type === 'email' ? (<ArrowDownAZ />) : (<ArrowUpAZ />)}
+        </button>
+    )
+}
