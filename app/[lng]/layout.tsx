78ca432a5a04134eafe1274fa158691f5f8df25f
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-17 18:47:16 +0300

    Create static metadata

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,10 +1,11 @@
 import { dir } from 'i18next'
+import { Metadata } from 'next';
 import { Nunito } from "next/font/google";
-import { languages, fallbackLng } from '../i18n/settings'
-import { useTranslation } from '../i18n'
+import { languages } from '../i18n/settings'
+import { Footer } from './components/footer';
 import Header from './components/header/header'
-import './globals.css'
 import { ThemeProvider } from './components/theme-provider';
+import './globals.css'
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -12,19 +13,10 @@ export async function generateStaticParams() {
     return languages.map((lng) => ({ lng }))
 }
 
-export async function generateMetadata({ params: { lng } }: {
-    params: {
-        lng: string;
-    };
-}) {
-    if (languages.indexOf(lng) < 0) lng = fallbackLng
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { t } = await useTranslation(lng)
-    return {
-        title: t('title'),
-        content: 'Тестовое задание FrontEnd Junior'
-    }
-}
+export const metadata: Metadata = {
+    title: "Creos CRM",
+    description: "Тестовое задание FrontEnd Junior",
+};
 
 interface RootLayoutProps {
     children: React.ReactNode,
@@ -40,18 +32,21 @@ export default function RootLayout({
     return (
         <html lang={lng} dir={dir(lng)}>
             <head />
-            <body className={`${nunito.className} p-3 lg:px-24 lg:py-12 min-h-screen bg-neutral-200 dark:bg-zinc-800 overflow-x-hidden`}>
+            <body className={`${nunito.className}`}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
                     enableSystem
                     disableTransitionOnChange
                 >
-                    <div className='space-y-12'>
-                        <Header lng={lng} />
-                        <main className='space-y-12'>
-                            {children}
-                        </main>
+                    <div className='flex flex-col justify-between gap-y-12 p-3 lg:px-12 xl:px-24 lg:py-12 min-h-screen bg-neutral-200 dark:bg-zinc-800 overflow-x-hidden'>
+                        <div className='space-y-12'>
+                            <Header lng={lng} />
+                            <main className='space-y-12'>
+                                {children}
+                            </main>
+                        </div>
+                        <Footer />
                     </div>
                 </ThemeProvider>
             </body>
