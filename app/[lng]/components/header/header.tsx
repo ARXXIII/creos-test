4f789c6fd7b7d0c75ce98a6db8a7c4f0bec8fb7f
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-17 18:36:05 +0300

    Added current work week

--- app/[lng]/components/header/header.tsx
+++ app/[lng]/components/header/header.tsx
@@ -3,18 +3,32 @@
 import Link from "next/link"
 
 import { Navbar } from "../nav/navbar"
+import { addHours, getISOWeek } from "date-fns"
+import { useTranslation } from "@/app/i18n/client"
 import { LangSwitcher } from "../switchers/lang-switcher"
 import { ThemeSwitcher } from "../switchers/theme-switcher"
 
 const Header = ({ lng }: { lng: string }) => {
+    const { t } = useTranslation(lng, 'header')
+
+    const adjustedDate = addHours(new Date(), 11);
+
+    const currentWorkWeek = getISOWeek(adjustedDate)
+
     return (
-        <header className="flex justify-between gap-x-6 relative">
-            <Link href='/' className="hidden lg:block font-black text-5xl">Creos</Link>
+        <header className="flex justify-between gap-x-3 lg:gap-x-6 relative">
+            <Link href='/' className="hidden md:block font-black text-neutral-500 dark:text-neutral-400 text-5xl">Creos</Link>
+            <div className="hidden md:flex items-center gap-x-3">
+                <h1 className="font-semibold text-neutral-500 dark:text-neutral-400">{t('currentWorkWeek')}</h1>
+                <div className="flex justify-center items-center w-12 h-12 rounded-lg shadow-neumorphism-light dark:shadow-neumorphism-dark">
+                    <p className="font-semibold">{currentWorkWeek}</p>
+                </div>
+            </div>
             <div className="flex items-start gap-x-6">
                 <LangSwitcher />
                 <ThemeSwitcher />
             </div>
-            <Navbar lng={lng} />
+            <Navbar currentWorkWeek={currentWorkWeek} lng={lng} />
         </header>
     )
 }
--- app/[lng]/components/nav/navbar.tsx
+++ app/[lng]/components/nav/navbar.tsx
@@ -6,13 +6,18 @@ import { NavLink } from './nav-link'
 import { motion } from 'framer-motion'
 import { NavButton } from './nav-button'
 
-export const Navbar = ({ lng }: { lng: string }) => {
+interface NavbarProps {
+    currentWorkWeek: number
+    lng: string
+}
+
+export const Navbar = ({ currentWorkWeek, lng }: NavbarProps) => {
     const [isActive, setIsActive] = useState<boolean>(false);
 
     const menu = {
         open: {
             width: "320px",
-            height: "300px",
+            height: "315px",
             top: "-10px",
             right: "-10px",
             transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] }
@@ -23,6 +28,17 @@ export const Navbar = ({ lng }: { lng: string }) => {
             top: "3px",
             right: "3px",
             transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1] }
+        }
+    }
+
+    const workWeek = {
+        open: {
+            scale: 1,
+            transition: { delay: 0.6 }
+        },
+        closed: {
+            scale: 0,
+            transition: { delay: 0.4 }
         }
     }
 
@@ -38,10 +54,26 @@ export const Navbar = ({ lng }: { lng: string }) => {
                 transition={{ type: 'spring', duration: 0.6 }}
                 className="absolute bg-neutral-200 dark:bg-zinc-800 rounded-xl shadow-neumorphism-light dark:shadow-neumorphism-dark overflow-hidden z-10"
             >
-                <div className='absolute top-[20%] left-6 space-y-12'>
+                <motion.div
+                    variants={workWeek}
+                    initial="closed"
+                    animate={isActive ? "open" : "closed"}
+                    className='flex justify-center items-center md:hidden absolute top-3 left-3 w-12 h-12 font-semibold rounded-lg shadow-neumorphism-light dark:shadow-neumorphism-dark'
+                >
+                    {currentWorkWeek}
+                </motion.div>
+                <div className='absolute top-[25%] left-3 space-y-12'>
 
                     {links.map((link, index) => (
-                        <NavLink key={link.title} index={index} title={link.title} href={link.href} lng={lng} isActive={isActive} toggleMenu={() => setIsActive(!isActive)} />
+                        <NavLink
+                            key={link.title}
+                            index={index}
+                            title={link.title}
+                            href={link.href}
+                            lng={lng}
+                            isActive={isActive}
+                            toggleMenu={() => setIsActive(!isActive)}
+                        />
                     ))}
 
                 </div>
